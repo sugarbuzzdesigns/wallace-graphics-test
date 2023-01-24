5515767890be9369d72849b48f665397e2659d08
JuStIn-B-WaRd <justinbward@protonmail.com>
2023-01-24 13:23:35 -0500

    ListJob Component Update
    Updated ListJob with several new features:
    
    - Data from jobs output into table on homepage
    - Title, edit & delete buttons, search bar added (no functionality)

--- client/src/components/ListJob.js
+++ client/src/components/ListJob.js
@@ -1,11 +1,11 @@
 import React, { Fragment, useEffect, useState } from "react";
 
 const ListJob = () => {
-  const [job, setJob] = useState([]);
+  const [job, setJob] = useState({});
 
   const getJob = async () => {
     try {
-      const response = await fetch("http://localhost:5000/job/ANTHONY");
+      const response = await fetch("http://localhost:5000/job/160876");
       const jsonData = await response.json();
 
       setJob(jsonData);
@@ -18,32 +18,56 @@ const ListJob = () => {
     getJob();
   }, []);
 
-  console.log(job);
 
   return (
     <Fragment>
+      <div><input type={"text"} placeholder={"Search"}></input></div>
+      <h1 className="text-center mt-5">Job List</h1>
       {" "}
       <table className="table mt-5 text-center">
         <thead>
           <tr>
+            <th>Job #</th>
+            <th>Customer ID</th>
+            <th>Customer Name</th>
+            <th>Legal Name</th>
+            <th>CSR ID</th>
+            <th>CSR Name</th>
+            <th>CSR Email</th>
+            <th>Sales ID</th>
+            <th>Sales Name</th>
+            <th>Sales Email</th>
             <th>Description</th>
-            <th>Edit</th>
-            <th>Delete</th>
+            <th>Description (Cont.)</th>
+            <th>Promise Date</th>
+            <th>Quantity</th>
+            <th>Final W</th>
+            <th>Final H</th>
           </tr>
         </thead>
         <tbody>
-          {/*<tr>
-            <td>John</td>
-            <td>Doe</td>
-            <td>john@example.com</td>
-          </tr> */}
-          {job.map(todo => (
-            <tr>
-              <td>{todo.description}</td>
-            </tr>
-          ))}
+          <tr>
+            <td>{job["JOB #"]}</td>
+            <td>{job["CUSTOMER ID"]}</td>
+            <td>{job["CUSTOMER NAME"]}</td>
+            <td>{job["LEGAL NAME"]}</td>
+            <td>{job["CSR ID"]}</td>
+            <td>{job["CSR NAME"]}</td>
+            <td>{job["CSR EMAIL"]}</td>
+            <td>{job["SALES ID"]}</td>
+            <td>{job["SALES NAME"]}</td>
+            <td>{job["SALES EMAIL"]}</td>
+            <td>{job["DESCRIPTION"]}</td>
+            <td>{job["DESCRIPTION (CONT.)"]}</td>
+            <td>{job["PROMISE DATE"]}</td>
+            <td>{job["QUANTITY"]}</td>
+            <td>{job["FINAL W"]}</td>
+            <td>{job["FINAL H"]}</td>
+          </tr>
         </tbody>
       </table>
+    <button>EDIT</button> <br></br><br></br>
+    <button>DELETE</button>
     </Fragment>
   );
 };
