5f3aa1a1ce0ce86d57ad1f201e8d647e914218a7
JuStIn-B-WaRd <justinbward@protonmail.com>
2023-01-24 10:24:47 -0500

    Uncaught TypeError: job.map is not a function
    Trying to get ListJob component to work. This bug is breaking it.

--- client/src/components/ListJob.js
+++ client/src/components/ListJob.js
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { Fragment, useEffect, useState } from "react";
 
 const ListJob = () => {
-    return <h1>Job Information</h1>;
+  const [job, setJob] = useState([]);
+
+  const getJob = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/job/ANTHONY");
+      const jsonData = await response.json();
+
+      setJob(jsonData);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  useEffect(() => {
+    getJob();
+  }, []);
+
+  console.log(job);
+
+  return (
+    <Fragment>
+      {" "}
+      <table className="table mt-5 text-center">
+        <thead>
+          <tr>
+            <th>Description</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/*<tr>
+            <td>John</td>
+            <td>Doe</td>
+            <td>john@example.com</td>
+          </tr> */}
+          {job.map(todo => (
+            <tr>
+              <td>{todo.description}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </Fragment>
+  );
 };
 
 export default ListJob;
