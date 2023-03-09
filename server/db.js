fb393c5eff057343bd779965984e550f1bd9ab26
JuStIn-B-WaRd <109784063+JuStIn-B-WaRd@users.noreply.github.com>
2023-03-09 14:35:48 -0500

    Update db.js
    Hid the password.

--- server/db.js
+++ server/db.js
@@ -5,7 +5,7 @@ dotenv.config({ path: "./.env" })
 
 const pool = new Pool({
   user: process.env.DATABASE_USER || "epace_read",
-  password: process.env.DATABASE_PASSWORD || "epace",
+  password: process.env.DATABASE_PASSWORD || "?",
   host: process.env.DATABASE_HOST || "10.0.100.7",
   port: 5432,
   database: process.env.DATABASE_NAME || "epace",
